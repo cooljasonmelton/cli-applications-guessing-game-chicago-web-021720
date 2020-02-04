f58a23a01c0369714606d019fdf245d48f1769d8
Jason Melton <jason.melton2@gmail.com>
2020-02-04 15:28:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,32 @@
-# Code your solution here!
+require "pry"
+
+def rand_number 
+  rand(5) + 1
+end 
+
+def prompt_user
+  puts  "Guess the number!"
+end
+  
+def get_user_input
+  gets
+end
+
+def compare_input
+  comp = rand_number
+  if get_user_input == "exit"
+    puts "Goodbye!"
+  elsif get_user_input == comp
+    puts "You guessed the correct number!"
+  else 
+    "Sorry! The computer guessed #{comp}."
+  end 
+end
+
+def run_guessing_game 
+  prompt_user
+  get_user_input
+  compare_input
+
+end 
+
